72c06df6fbb6035086d4ecb074f37307a6b0ccc1
xmNuc <xmnuc@o2.pl>
2021-11-02 22:19:21 +0000

    1.3 commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { urlencoded } = require('express');
 const express = require('express');
 const hbs = require('express-handlebars');
+const { clientRouter } = require('./routers/client');
+const { homeRouter } = require('./routers/home');
 
 const app = express();
 
@@ -15,9 +16,8 @@ app.engine(
 );
 app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
-  res.render('test');
-});
+app.use('/', homeRouter);
+app.use('/client', clientRouter);
 
 app.listen(3000, 'localhost', () => {
   console.log('Starting server on http://localhost:3000');
--- routers/client.js
+++ routers/client.js
@@ -1,4 +1,4 @@
-const express = reqiure('express');
+const express = require('express');
 
 const clientRouter = express.Router();
 
