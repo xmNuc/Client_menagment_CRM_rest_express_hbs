b316e0410759b698022dc0e941ba85885b5abfef
xmNuc <xmnuc@o2.pl>
2021-11-02 23:47:24 +0000

    1.5 commit

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,15 @@ const { join } = require('path');
 class Db {
   constructor(dbFileName) {
     this.dbFileName = join(__dirname, '../data', dbFileName);
-    console.log(this.dbFileName);
+    this._load();
+  }
+  async _load() {
+    this._data = JSON.parse(await readFile(this.dbFileName, 'utf8'));
+    // console.log(this._data);
+  }
+  create(obj) {
+    this._data.push(obj);
+    writeFile(this.dbFileName, JSON.stringify(this._data), 'utf8');
   }
 }
 
