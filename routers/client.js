922f28aabc0d2243ecc65f6eb79313b3f35514ec
xmNuc <xmnuc@o2.pl>
2021-11-04 23:54:17 +0000

    1.8 commit

--- routers/client.js
+++ routers/client.js
@@ -6,7 +6,7 @@ const clientRouter = express.Router();
 clientRouter
   .get('/', (req, res) => {
     res.render('client/list-all', {
-      clients: db.getAll,
+      clients: db.getAll(),
     });
   })
   .get('/:id', (req, res) => {
