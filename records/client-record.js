4a29784c11dbf98ddac33b5981884bf654ce705e
xmNuc <xmnuc@o2.pl>
2021-11-16 03:44:25 +0100

    2.7 commit

--- records/client-record.js
+++ records/client-record.js
@@ -1,16 +1,32 @@
 class ClientRecord {
   constructor(obj) {
-    const { name } = obj;
+    const { id, name, mail, nextContactAt, notes } = obj;
+
+    if (!id || typeof id !== 'string') {
+      throw new Error(' ID must be added and have to be text');
+    }
 
     if (!name || typeof name !== 'string' || name.length < 3) {
       throw new Error(' Name should have min 3 letters');
     }
 
-    this.id = obj.id;
-    this.name = obj.name;
-    this.mail = obj.mail;
-    this.nextContactAt = obj.nextContactAt;
-    this.notes = obj.notes;
+    if (!mail || typeof mail !== 'string' || mail.indexOf('@') === -1) {
+      throw new Error('E-mail is not valid');
+    }
+
+    if (typeof nextContactAt !== 'string') {
+      throw new Error('Date of next contact have to be text');
+    }
+
+    if (typeof notes !== 'string') {
+      throw new Error('Notes have to be text');
+    }
+
+    this.id = id;
+    this.name = name;
+    this.mail = mail;
+    this.nextContactAt = nextContactAt;
+    this.notes = notes;
   }
 }
 
